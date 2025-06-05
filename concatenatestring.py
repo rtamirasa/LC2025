0f311be36712cc51122ad3071ed2d3ab4b66b2f6
Ridhi Tamirasa <49322479+rtamirasa@users.noreply.github.com>
2025-06-05 10:37:18 -0700

    Create concatenatestring.py

---
+++ concatenatestring.py
@@ -0,0 +1,7 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        
+        print(nums)
+        nums2 = nums
+        new_array = nums+ nums2
+        return new_array
